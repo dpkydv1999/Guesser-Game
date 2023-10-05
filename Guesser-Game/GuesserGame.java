2730c16884bbf1d549d8523dce4e17c9a08df827
dpkydv1999 <ydv.dpk99@gmail.com>
2023-10-05 08:41:09 +0545

    Initial commit

---
+++ Guesser-Game/GuesserGame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Guesser{
+    int Guessernum;
+
+    public int takeNumberGuesser(){
+        Scanner scan = new Scanner(System.in);
+        Guessernum = scan.nextInt();
+        return Guessernum;
+    }
+}
+class Player{
+
+}
+class Umpire{
+
+}
+
+public class GuesserGame{
+    public static void main(String[] args) {
+        
+    }
+
+}
